1dcbafa816e0098af0b5e2e3ac71380b5319e5d8
bdendro <olegburia2072@gmail.com>
2026-04-08 17:48:54 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    ignores: ['eslint.config.mjs', 'dist', '**/dist/**', '**/node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
+  {
+    rules: {
+      indent: 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-warning-comments': ['warn', { terms: ['todo', 'fixme'], location: 'anywhere' }],
+    },
+  },
+  eslintConfigPrettier,
+]);
